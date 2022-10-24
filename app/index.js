1bbb5f12f956df24800e91231c9e12da7080da2c
susukida <susukida@loremipsum.co.jp>
2022-10-24 14:49:26 +0900

    Add registering event handlers

--- app/index.js
+++ app/index.js
@@ -1,8 +1,13 @@
+import { me as appbit } from "appbit";
 import * as document from "document";
 import * as fs from "fs";
+import { HeartRateSensor } from "heart-rate";
 import * as messaging from "messaging";
 
+appbit.appTimeoutEnabled = false;
+
 const SETTINGS_FILE = "settings.json";
+const RESEND_INTERVAL = 5000;
 
 const el = {
   currentRelax: document.getElementById("currentRelax"),
@@ -19,14 +24,20 @@ let state = {
 };
 
 updateSettings(loadSettings());
+registerHandlers();
 
-messaging.peerSocket.addEventListener("message", (event) => {
-  if (event && event.data && event.data.type === "settings") {
-    const { settings } = event.data;
-    updateSettings(settings);
-    saveSettings(settings);
+function registerHandlers() {
+  if (HeartRateSensor) {
+    const hrm = new HeartRateSensor({ frequency: 1 });
+
+    hrm.addEventListener("reading", () => onReading(hrm.heartRate));
+    hrm.start();
   }
-});
+
+  messaging.peerSocket.addEventListener("message", event => onMessage(event));
+
+  setInterval(onTimeout, RESEND_INTERVAL);
+}
 
 function loadSettings() {
   const defaultSettings = {
@@ -63,4 +74,21 @@ function updateSettings(settings) {
   el.thresholdLow.text = `低しきい値：${settings.thresholdLow.toFixed(3)}`;
   el.retentionPeriod.text = `保持期間：${settings.retentionPeriod}`;
   el.sendHttp.text = `HTTP送信：${settings.sendHttp ? "ON" : "OFF"}`;
+}
+
+function onReading(heartRate) {
+  if (!heartRate) {
+    return;
+  }
+}
+
+function onMessage(event) {
+  if (event && event.data && event.data.type === "settings") {
+    const { settings } = event.data;
+    updateSettings(settings);
+    saveSettings(settings);
+  }
+}
+
+function onTimeout() {
 }
